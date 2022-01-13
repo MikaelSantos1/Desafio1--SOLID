8b45bd9405eaf516cb759f5c347100b3abb52870
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-13 16:54:45 -0300

    fixed all

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -11,11 +11,12 @@ class ListAllUsersController {
             if (!user_id) {
                 return response.json({ error: "Id not proveded" }).status(404);
             }
+
             const id = user_id.toString();
             const all = this.listAllUsersUseCase.execute({ user_id: id });
             return response.json(all);
         } catch (err) {
-            return response.json({ error: err.message }).status(404);
+            return response.status(400).json({ error: err.message });
         }
     }
 }
