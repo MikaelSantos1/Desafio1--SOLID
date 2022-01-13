18a59415ac49df83a924b69aff7140b0e742b2de
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-13 16:20:56 -0300

    fixing routes

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,21 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        try {
+            const { user_id } = request.headers;
+            if (!user_id) {
+                return response.json({ error: "Id not proveded" }).status(404);
+            }
+            const id = user_id.toString();
+            const all = this.listAllUsersUseCase.execute({ user_id: id });
+            return response.json(all);
+        } catch (err) {
+            return response.json({ error: err.message }).status(404);
+        }
+    }
 }
 
 export { ListAllUsersController };
